444e359d3fa003c0e038cfbecb4669884fc760a8
Jason Jordan <jtjordan13@gmail.com>
2016-06-06 15:29:13 -0400

    Hangman keeps track of wins and losses, and displays that to user

--- hangman.rb
+++ hangman.rb
@@ -13,11 +13,23 @@ def player_wants_to_quit?
   end
 end
 
-def game_over? board
-  # !board.include?("_")
-  if board.include? "_"
+def game_over? board, attempts, username
+  user_id = User.where(username: username).ids.first.to_s
+  if (board.include? "_") && (attempts > 0)
     false
+  elsif attempts == 0
+    puts "You ran out of attempts!"
+    loss = Record.where(user_id: user_id).first_or_create!
+    loss.losses += 1
+    loss.save!
+    show_record username
+    true
   else
+    puts "You win!"
+    win = Record.where(user_id: user_id).first_or_create!
+    win.wins += 1
+    win.save!
+    show_record username
     true
   end
 end
@@ -58,6 +70,21 @@ def record_move l, answer, grid, tries
   return tries
 end
 
+def get_username
+  puts "What name do you want to go by?"
+  username = gets.chomp
+end
+
+def show_record username
+  user = User.where(username: username).first_or_create!
+  wins = Record.where(user_id: user.id.to_s).first_or_create!.wins
+  losses = Record.where(user_id: user.id.to_s).first_or_create!.losses
+  puts "#{username}'s record: #{wins} - wins and #{losses} - losses"
+end
+
+puts "Welcome to Hangman!"
+username = get_username
+show_record username
 loop do
   word = get_a_word
   warn "The word is: #{word}"
@@ -65,7 +92,7 @@ loop do
   attempts = 6
 
   # Play game one time
-  until game_over? b
+  until game_over? b, attempts, username
     display_board b
     letter = choose_letter
     attempts = record_move letter, word, b, attempts
